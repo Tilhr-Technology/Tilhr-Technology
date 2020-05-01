7def4062a1f2b392edd1906a24cb1fc9270b275d
tilhr <ping@tylerrodgers.space>
2020-05-01 00:49:21 -0700

    Update news stuff

--- Website/1.3.8(beta)/js/main.js
+++ Website/1.3.8(beta)/js/main.js
@@ -1,3 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 jQuery(function($) {
 	"use strict";
 
--- Website/1.3.8(beta)/news/Readme.txt
+++
@@ -1,6 +0,0 @@
-Thanks for downloading this theme!
-
-Theme Name: Plato
-Theme URL: https://bootstrapmade.com/plato-responsive-bootstrap-website-template/
-Author: BootstrapMade.com
-Author URL: https://bootstrapmade.com
